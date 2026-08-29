f107eb0d9be2b12dfbf6849163e992952aa6cd15
Peter Daae <peterhorndaae@gmail.com>
2026-08-29 10:15:09 +0200

    fixed example data for test

--- first_project/utils/exampleProfileData.ts
+++ first_project/utils/exampleProfileData.ts
@@ -1,5 +1,51 @@
 import { ProfileData } from "./profileData";
 
-const profile: ProfileData[] = [
+
+/*
+
+    id: String,
+    name: String,
+    age: String,
+    bio: String,
+    jobTitle: String
     
+    */
+
+const profile: ProfileData[] = [
+ {  id: "1",
+    name: "Person1",
+    age: "30",
+    bio: "story about me",
+    jobTitle: "job"
+ },
+  {  id: "2",
+    name: "Person2",
+    age: "30",
+    bio: "story about me",
+    jobTitle: "job"
+ },
+  {  id: "3",
+    name: "Person3",
+    age: "30",
+    bio: "story about me",
+    jobTitle: "job"
+ },
+  {  id: "4",
+    name: "Person4",
+    age: "30",
+    bio: "story about me",
+    jobTitle: "job"
+ },
+  {  id: "5",
+    name: "Person5",
+    age: "30",
+    bio: "story about me",
+    jobTitle: "job"
+ },
+  {  id: "6",
+    name: "Person6",
+    age: "30",
+    bio: "story about me",
+    jobTitle: "job"
+ }
 ]
